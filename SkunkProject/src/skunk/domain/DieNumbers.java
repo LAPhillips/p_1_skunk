c4fcfe74380dfad534a9635f12ae047310a4112d
LAPhillips <phil5544@stthomas.edu>
2020-10-24 14:57:23 -0500

    DieNumbers Pass 10.24.2020 14:57

---
+++ SkunkProject/src/skunk/domain/DieNumbers.java
@@ -0,0 +1,10 @@
+package skunk.domain;
+
+public class DieNumbers {
+
+	public static int randomOneToSix() {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+}
---
+++ SkunkProject/test/skunk/domain/DieNumbersTest.java
@@ -0,0 +1,15 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DieNumbersTest {
+
+	@Test
+	void die_should_return_number_between_one_and_six() {
+		int result = DieNumbers.randomOneToSix();
+		assertTrue(result < 7 && result > 0);
+	}
+
+}
