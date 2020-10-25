ff8048166382ef5f4c456ca9575ce2f55b2fc9e7
LAPhillips <phil5544@stthomas.edu>
2020-10-24 19:06:30 -0500

    Arrays Match - Fixed Array for Dice Passed

--- SkunkProject/test/skunk/domain/DiceTest.java
+++ SkunkProject/test/skunk/domain/DiceTest.java
@@ -2,6 +2,7 @@ package skunk.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class DiceTest {
@@ -25,10 +26,23 @@ class DiceTest {
 	@Test
 	void dice_should_produce_two_fixed_numbers_from_array() {
 		Dice dice = new Dice();
-		int [] diceArray = {2,4,6,8};
-		dice.roll(diceArray);
+		int [] fixedArray = {2,4,6,8};
+		dice.roll(fixedArray);
 		int result = dice.getLastRoll();
 		assertTrue(result == 4);
+	}
+	
+	@Test
+	void dice_should_produce_two_fixed_numbers_from_array_in_roll_after_roll() {
+		Dice dice = new Dice();
+		int [] fixedArray = {2,4,6,8};
+		int [] actualRolls = {0,0,0,0};
+		for (int i = 0; i < fixedArray.length; i++) {
+			dice.roll(fixedArray);
+			int result = dice.getLastRoll();
+			actualRolls[i] = result/2;
+		}
+		Assert.assertArrayEquals(fixedArray, actualRolls);
 	}
 	
 
