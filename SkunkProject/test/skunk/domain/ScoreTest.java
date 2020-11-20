3cac6728fe7dd27e52a565cecf0f9c16aaa16495
LAPhillips <phil5544@stthomas.edu>
2020-11-20 11:43:13 -0600

    Passed: Turn Getters

--- SkunkProject/test/skunk/domain/ScoreTest.java
+++ SkunkProject/test/skunk/domain/ScoreTest.java
@@ -11,40 +11,22 @@ class ScoreTest {
 	@Test
 	void score_shares_scoreboard() {
 		Score score = new Score();
-		int[]scores = new int[] {5,3};
-		score.recordScore(scores);
-		ArrayList<Integer> retreivedScore = score.getScoreboard();
-		assertEquals(scores[1], retreivedScore.get(1));
-	}	
+		ArrayList<Integer> scores = score.getScoreboard();
+		assertNotNull(scores);
+	}
 	
 	@Test
-	void score_keeps_a_running_tally_for_turn() {
-		Score score = new Score();
-		int expected1 = 4;
-		int expected2 = 5;
-		int [] firstRoll = new int[] {2,3};
-		score.recordScore(firstRoll);
-		int [] secondRoll = new int[] {expected1, expected2};
-		score.recordScore(secondRoll);
-		int expected = expected1 + expected2 + 2 + 3;
-		int result = score.allRollsOneTurn();
-		assertEquals(expected, result);
-	}	
-	
-	@Test
-	void score_can_get_final_score() {
+	void score_shares_FinalScore() {
 		Score score = new Score();
 		int finalScore = score.getFinalScore();
-		assertEquals(0, finalScore);
-	}	
-
-	@Test
-	void score_gives_a_cumulative_score() {
-		Score score = new Score();
-		int expected = 10;
-		int[]scores = new int[] {5,5};
-		score.recordScore(scores);
-		int result = score.getFinalScore();
-		assertEquals(expected, result);
+		assertEquals(0, finalScore); //expect default to be 0
 	}
+	
+	@Test
+	void score_shares_turnScore() {
+		Score score = new Score();
+		int turnScore = score.getTurnScore();
+		assertEquals(0, turnScore); //expect default to be 0
+	}
+
 }
